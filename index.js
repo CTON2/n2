118bad93493dbe24a343927b7f3bb2dab1f7c4ac
soupysoupsouperson <78237337+soupysoupsouperson@users.noreply.github.com>
2021-02-16 21:30:06 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-{
-  "name": "apptest3",
-  "version": "1.0.0",
-  "description": "",
-  "main": "index.js",
-  "scripts": {
-    "test": "echo \"Error: no test specified\" && exit 1"
-  },
-  "keywords": [],
-  "author": "",
-  "license": "ISC",
-  "dependencies": {
-    "@cto.ai/sdk": "^2.4.1"
+const { sdk } = require('@cto.ai/sdk')
+const http = require('http')
+async function main() {
+  const requestListener = function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'application/json' })
+    res.write('Hello World!')
+    res.end()
   }
+  const server = http.createServer(requestListener)
+  server.listen(8080, function () {
+    sdk.log('server started!')
+  })
 }
+main()
